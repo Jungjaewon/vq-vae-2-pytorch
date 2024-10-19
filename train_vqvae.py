f0ccfa0baf313444f8f00a997627cf60fa6557fe
jungjaewon <woodcook486@naver.com>
2024-10-20 00:14:32 +0900

    add single gpu

--- train_vqvae.py
+++ train_vqvae.py
@@ -5,19 +5,14 @@ import os
 import torch
 from torch import nn, optim
 from torch.utils.data import DataLoader
-
 from torchvision import datasets, transforms, utils
-
 from tqdm import tqdm
 
 from vqvae import VQVAE
 from scheduler import CycleScheduler
-import distributed as dist
-
 
 def train(epoch, loader, model, optimizer, scheduler, device):
-    if dist.is_primary():
-        loader = tqdm(loader)
+    loader = tqdm(loader)
 
     criterion = nn.MSELoss()
 
@@ -42,49 +37,45 @@ def train(epoch, loader, model, optimizer, scheduler, device):
             scheduler.step()
         optimizer.step()
 
-        part_mse_sum = recon_loss.item() * img.shape[0]
-        part_mse_n = img.shape[0]
-        comm = {"mse_sum": part_mse_sum, "mse_n": part_mse_n}
-        comm = dist.all_gather(comm)
+        mse_sum += recon_loss.item() * img.shape[0]
+        mse_n += img.shape[0]
 
-        for part in comm:
-            mse_sum += part["mse_sum"]
-            mse_n += part["mse_n"]
+        lr = optimizer.param_groups[0]["lr"]
 
-        if dist.is_primary():
-            lr = optimizer.param_groups[0]["lr"]
+        loader.set_description(
+            (
+                f"epoch: {epoch + 1}; mse: {recon_loss.item():.5f}; "
+                f"latent: {latent_loss.item():.3f}; avg mse: {mse_sum / mse_n:.5f}; "
+                f"lr: {lr:.5f}"
+            )
+        )
 
-            loader.set_description(
-                (
-                    f"epoch: {epoch + 1}; mse: {recon_loss.item():.5f}; "
-                    f"latent: {latent_loss.item():.3f}; avg mse: {mse_sum / mse_n:.5f}; "
-                    f"lr: {lr:.5f}"
-                )
+        if i % 100 == 0:
+            model.eval()
+
+            sample = img[:sample_size]
+
+            with torch.no_grad():
+                out, _ = model(sample)
+
+            utils.save_image(
+                torch.cat([sample, out], 0),
+                f"sample/{str(epoch + 1).zfill(5)}_{str(i).zfill(5)}.png",
+                nrow=sample_size,
+                normalize=True,
+                range=(-1, 1),
             )
 
-            if i % 100 == 0:
-                model.eval()
-
-                sample = img[:sample_size]
-
-                with torch.no_grad():
-                    out, _ = model(sample)
-
-                utils.save_image(
-                    torch.cat([sample, out], 0),
-                    f"sample/{str(epoch + 1).zfill(5)}_{str(i).zfill(5)}.png",
-                    nrow=sample_size,
-                    normalize=True,
-                    range=(-1, 1),
-                )
-
-                model.train()
+            model.train()
 
 
 def main(args):
-    device = "cuda"
-
-    args.distributed = dist.get_world_size() > 1
+    # Specify GPU by number
+    if torch.cuda.is_available():
+        torch.cuda.set_device(args.gpu)
+        device = f"cuda:{args.gpu}"
+    else:
+        device = "cpu"
 
     transform = transforms.Compose(
         [
@@ -96,19 +87,11 @@ def main(args):
     )
 
     dataset = datasets.ImageFolder(args.path, transform=transform)
-    sampler = dist.data_sampler(dataset, shuffle=True, distributed=args.distributed)
     loader = DataLoader(
-        dataset, batch_size=128 // args.n_gpu, sampler=sampler, num_workers=2
+        dataset, batch_size=128, shuffle=True, num_workers=2
     )
 
     model = VQVAE().to(device)
-
-    if args.distributed:
-        model = nn.parallel.DistributedDataParallel(
-            model,
-            device_ids=[dist.get_local_rank()],
-            output_device=dist.get_local_rank(),
-        )
 
     optimizer = optim.Adam(model.parameters(), lr=args.lr)
     scheduler = None
@@ -124,20 +107,14 @@ def main(args):
     for i in range(args.epoch):
         train(i, loader, model, optimizer, scheduler, device)
 
-        if dist.is_primary():
-            torch.save(model.state_dict(), f"checkpoint/vqvae_{str(i + 1).zfill(3)}.pt")
+        torch.save(model.state_dict(), f"checkpoint/vqvae_{str(i + 1).zfill(3)}.pt")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--n_gpu", type=int, default=4)
 
-    port = (
-        2 ** 15
-        + 2 ** 14
-        + hash(os.getuid() if sys.platform != "win32" else 1) % 2 ** 14
-    )
-    parser.add_argument("--dist_url", default=f"tcp://127.0.0.1:{port}")
+    # Add GPU argument
+    parser.add_argument("--gpu", type=int, default=0, help="GPU number to use")
 
     parser.add_argument("--size", type=int, default=256)
     parser.add_argument("--epoch", type=int, default=1000)
@@ -149,4 +126,4 @@ if __name__ == "__main__":
 
     print(args)
 
-    dist.launch(main, args.n_gpu, 1, 0, args.dist_url, args=(args,))
+    main(args)
